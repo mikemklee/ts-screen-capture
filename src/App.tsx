3a8505bf07f835a94bac2f720f2859c1cc21de93
Mike Lee <mike@mosaicmanufacturing.com>
2020-03-23 18:27:37 -0400

    Initial commit; setup electron forge with typescript and webpack and add react

---
+++ src/App.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import './index.css';
+
+ReactDOM.render(<div>hello YO from React! </div>, document.getElementById('root'));
